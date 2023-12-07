137484c2dd026ade9627f6937ed50b2c678bfb9b
chethan <chetangowda4c@gmail.com>
2023-12-07 21:10:45 +0530

    Extent report is added.

---
+++ src/main/java/com/shoppersstacks/qa/util/ExtentReport.java
@@ -0,0 +1,62 @@
+package com.shoppersstacks.qa.util;
+
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ExtentReport extends TestListenerAdapter {
+    public ExtentSparkReporter sparkReporter;
+    public ExtentReports extentReports;
+    public ExtentTest extentTest;
+    public void onStart(ITestContext testContext) {
+        String timeStamp=new SimpleDateFormat("yyyy.MM.dd.hh.mm.ss").format(new Date());
+        String repName="Test-Report-"+timeStamp+",html";
+
+        sparkReporter=new ExtentSparkReporter("./TestOutput/"+repName);
+        try {
+            sparkReporter.loadXMLConfig("./src/extent-config.xml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        extentReports=new ExtentReports();
+        extentReports.attachReporter(sparkReporter);
+        extentReports.setSystemInfo("Host Name","LocalHost");
+        extentReports.setSystemInfo("Environment","QA");
+        extentReports.setSystemInfo("user","Chethan");
+
+        sparkReporter.config().setDocumentTitle("ShoppersStack Test Project");
+        sparkReporter.config().setReportName("End To End Report");
+        sparkReporter.config().setTheme(Theme.DARK);
+    }
+
+    public void onTestSuccess(ITestResult testResult){
+        extentTest=extentReports.createTest(testResult.getName());
+        extentTest.log(Status.PASS, MarkupHelper.createLabel(testResult.getName(), ExtentColor.GREEN));
+    }
+    public void onTestFailure(ITestResult testResult){
+        extentTest=extentReports.createTest(testResult.getName());
+        extentTest.log(Status.FAIL, MarkupHelper.createLabel(testResult.getName(), ExtentColor.RED));
+    }
+
+    public void onTestSkipped(ITestResult testResult){
+        extentTest=extentReports.createTest(testResult.getName());
+        extentTest.log(Status.SKIP,MarkupHelper.createLabel(testResult.getName(),ExtentColor.ORANGE));
+    }
+
+    public void onFinish(ITestResult testContext){
+        extentReports.flush();
+    }
+}
